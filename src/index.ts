683149f1497a5d08716b39c1ab87ff244c015c12
DeveloperSky95 <haneulgim95@gmail.com>
2026-03-25 14:05:52 +0900

    README.md 생성, /posts 라우팅 생성

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,42 @@ dotenv.config();
 // 2. Express 앱 생성
 const app = express();
 app.use(express.json());    // app.use() 메소드는 미들웨어(중간에 끼워 넣는 프로그램)를 사용하게 할 때 사용
+                            // 여기서 미들웨어는 express.json()이다.
                             // x-ray라던지 방사능 검사기라 던지..
+                            // express.json() = "클라이언트가 보낸 JSON을 읽기 쉽게 변환해주는 도구"
+
+/*
+👉 "요청(Request)의 body를 JSON 형태로 해석해주는 미들웨어를 등록하는 것"
+
+const app = express();
+app.use(express.json());
+
+이 코드의 의미:
+
+express.json() 👉 JSON 파싱 미들웨어
+app.use(...) 👉 모든 요청에 이 기능 적용
+
+즉,
+👉 클라이언트가 보내는 JSON 데이터를
+👉 자동으로 req.body에 넣어준다
+
+전체 흐름은 이거야:
+
+[프론트] → JSON 데이터 전송
+        ↓
+[서버] express.json()이 해석
+        ↓
+req.body에 담김
+        ↓
+그걸 가지고 DB에 저장 or 로직 처리
+
+
+💡 한 줄 정리
+👉 express.json() = "클라이언트가 보낸 JSON을 읽기 쉽게 변환해주는 도구"
+ */
+
+
+
 
 
 // 데이터베이스가 없어서 임시로 만든 데이터 변수 (mock)
@@ -31,6 +66,7 @@ app.get("/hello", (req, res) => {
     res.send("여기는 hello 주소로 들어왔습니다.");
 })
 
+// 세번째 일꾼
 app.get("/posts", (req, res) => {
     // res.json(보낼데이터) 메소드 : string이 아닌, 객체 타입의 데이터(단, 함수 빼고) 를 보낼 때 사용
     // res.json 메소드를 사용하려면 app.use(express.json()) 을 꼭  써줘야 함
